9a7010ad951b71c74e0b351c54bb6fe77ae23f28
Rashi Awasthi <rashiawasthi260@gmail.com>
2026-03-11 17:47:26 +0530

    Updates

--- _next/static/duPVDKu6M9edm4DMy-yeV/_buildManifest.js
+++ _next/static/duPVDKu6M9edm4DMy-yeV/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nerve-assignment//_next/:path+",
+        "destination": "/nerve-assignment/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
