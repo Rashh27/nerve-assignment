944ba0f1a18566c14a046521f6415ff5ac412f4e
Rashi Awasthi <rashiawasthi260@gmail.com>
2026-03-11 22:34:47 +0530

    final deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
+  basePath: '/nerve-assignment',
+  assetPrefix: '/nerve-assignment',
+  trailingSlash: true, // This helps with routing on GitHub Pages
   images: {
     unoptimized: true,
   },
